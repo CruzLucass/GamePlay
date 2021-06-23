07c0f3cad79fa5b153c1f5a4180cf191a3c62505
LucasCruz <71300702+Lucas-PCruz@users.noreply.github.com>
2021-06-22 21:27:13 -0300

    aula02

---
+++ src/components/ButtonAdd/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
+
+import { styles } from './styles';
+import { theme } from "../../global/styles/theme";
+
+type Props = RectButtonProps & {
+    title: string;
+}
+
+export function ButtonAdd({ ...rest }: RectButtonProps) {
+    return (
+        <RectButton
+            style={styles.container}
+            {...rest}
+        >
+            <MaterialCommunityIcons
+                name="plus"
+                color={theme.colors.heading}
+                size={24}
+            />
+        </RectButton>
+    );
+}
--- src/screens/signin/index.tsx
+++ src/screens/signin/index.tsx
@@ -2,32 +2,35 @@ import React from 'react';
 import {
     View,
     Text,
-    Image,
-    StatusBar
+    Image
 } from 'react-native';
 
-import { ButtonIcon } from '../../components/ButtonIcon/Index';
-import IllustrationImg from '../../assets/illustration.png';
+import { ButtonIcon } from '../../components/ButtonIcon/index';
+import IllustrationImg from '../../../assets/illustration.png';
 import { styles } from './styles';
+import { useNavigation } from '@react-navigation/native';
 
 export function SignIn() {
+    const navigation = useNavigation();
+
+
+    function handleSignIn() {
+        navigation.navigate('Home');
+    }
+
     return (
+
         <View style={styles.container}>
-            <StatusBar
-                barStyle="light-content"
-                backgroundColor="transparent"
-                translucent
-            />
             <Image
                 source={IllustrationImg}
                 style={styles.image}
-                resizeMode='stretch'
+                resizeMode="stretch"
             />
             <View style={styles.content}>
                 <Text style={styles.title}>
-                    Organize {'\n'}
-                    suas jogatinas {'\n'}
-                    facilmente
+                    Conecte-se  {'\n'}
+                    e organize {'\n'}
+                    suas jogatinas
                 </Text>
                 <Text style={styles.subtitle}>
                     Crie grupos para jogar seus games  {'\n'}
@@ -36,7 +39,7 @@ export function SignIn() {
 
                 <ButtonIcon
                     title="Entrar com Discord"
-                    activeOpacity={0.7}
+                    onPress={handleSignIn}
                 />
             </View>
         </View>
